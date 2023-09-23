9f6334eb19f0235a48c71285a702aa6d629c6da7
chenhang233 <1973433180@qq.com>
2023-09-23 20:41:20 +0800

    prev: 12 cg assembly

--- src/12.types_1/cg.c
+++ src/12.types_1/cg.c
@@ -87,3 +87,139 @@ void cgglobsym(int id)
     else
         fprintf(Outfile, "\t.comm\t%s,1,1\n", Gsym[id].name);
 }
+
+int cgloadint(int value, int type)
+{
+    // Get a new register
+    int r = alloc_register();
+
+    fprintf(Outfile, "\tmovq\t$%d, %s\n", value, reglist[r]);
+    return (r);
+}
+
+int cgloadglob(int id)
+{
+    // Get a new register
+    int r = alloc_register();
+
+    // Print out the code to initialise it: P_CHAR or P_INT
+    if (Gsym[id].type == P_INT)
+        fprintf(Outfile, "\tmovq\t%s(\%%rip), %s\n", Gsym[id].name, reglist[r]);
+    else
+        fprintf(Outfile, "\tmovzbq\t%s(\%%rip), %s\n", Gsym[id].name, reglist[r]);
+    return (r);
+}
+
+// Add two registers together and return
+// the number of the register with the result
+int cgadd(int r1, int r2)
+{
+    fprintf(Outfile, "\taddq\t%s, %s\n", reglist[r1], reglist[r2]);
+    free_register(r1);
+    return (r2);
+}
+
+// Subtract the second register from the first and
+// return the number of the register with the result
+int cgsub(int r1, int r2)
+{
+    fprintf(Outfile, "\tsubq\t%s, %s\n", reglist[r2], reglist[r1]);
+    free_register(r2);
+    return (r1);
+}
+
+// Multiply two registers together and return
+// the number of the register with the result
+int cgmul(int r1, int r2)
+{
+    fprintf(Outfile, "\timulq\t%s, %s\n", reglist[r1], reglist[r2]);
+    free_register(r1);
+    return (r2);
+}
+
+// Divide the first register by the second and
+// return the number of the register with the result
+int cgdiv(int r1, int r2)
+{
+    fprintf(Outfile, "\tmovq\t%s,%%rax\n", reglist[r1]);
+    fprintf(Outfile, "\tcqo\n");
+    fprintf(Outfile, "\tidivq\t%s\n", reglist[r2]);
+    fprintf(Outfile, "\tmovq\t%%rax,%s\n", reglist[r1]);
+    free_register(r2);
+    return (r1);
+}
+
+void cgprintint(int r)
+{
+    fprintf(Outfile, "\tmovq\t%s, %%rdi\n", reglist[r]);
+    fprintf(Outfile, "\tcall\tprintint\n");
+    free_register(r);
+}
+
+int cgstorglob(int r, int id)
+{
+    // Choose P_INT or P_CHAR
+    if (Gsym[id].type == P_INT)
+        fprintf(Outfile, "\tmovq\t%s, %s(\%%rip)\n", reglist[r], Gsym[id].name);
+    else
+        fprintf(Outfile, "\tmovb\t%s, %s(\%%rip)\n", breglist[r], Gsym[id].name);
+    return (r);
+}
+
+// List of comparison instructions,
+// in AST order: A_EQ, A_NE, A_LT, A_GT, A_LE, A_GE
+static char *cmplist[] =
+    {"sete", "setne", "setl", "setg", "setle", "setge"};
+
+int cgcompare_and_set(AST_node_type ASTop, int r1, int r2)
+{
+
+    // Check the range of the AST operation
+    if (ASTop < A_EQ || ASTop > A_GE)
+        custom_error_int("Bad ASTop in cgcompare_and_set()", ASTop);
+
+    fprintf(Outfile, "\tcmpq\t%s, %s\n", reglist[r2], reglist[r1]);
+    fprintf(Outfile, "\t%s\t%s\n", cmplist[ASTop - A_EQ], breglist[r2]);
+    fprintf(Outfile, "\tmovzbq\t%s, %s\n", breglist[r2], reglist[r2]);
+    free_register(r1);
+    return (r2);
+}
+
+// Generate a label
+void cglabel(int l)
+{
+    fprintf(Outfile, "L%d:\n", l);
+}
+
+// Generate a jump to a label
+void cgjump(int l)
+{
+    fprintf(Outfile, "\tjmp\tL%d\n", l);
+}
+
+// List of inverted jump instructions,
+// in AST order: A_EQ, A_NE, A_LT, A_GT, A_LE, A_GE
+static char *invcmplist[] = {"jne", "je", "jge", "jle", "jg", "jl"};
+
+// Compare two registers and jump if false.
+int cgcompare_and_jump(AST_node_type ASTop, int r1, int r2, int label)
+{
+
+    // Check the range of the AST operation
+    if (ASTop < A_EQ || ASTop > A_GE)
+        custom_error_int("Bad ASTop in cgcompare_and_set()", ASTop);
+
+    fprintf(Outfile, "\tcmpq\t%s, %s\n", reglist[r2], reglist[r1]);
+    fprintf(Outfile, "\t%s\tL%d\n", invcmplist[ASTop - A_EQ], label);
+    freeall_registers();
+    return (NOREG);
+}
+
+// Widen the value in the register from the old
+// to the new type, and return a register with
+// this new value
+int cgwiden(int r, int oldtype, int newtype)
+{
+    // Nothing to do
+    return (r);
+}
--- src/12.types_1/function.h
+++ src/12.types_1/function.h
@@ -47,6 +47,19 @@ void cgfuncpreamble(char *name);
 void cgfuncpostamble();
 void cgprintint(int r);
 void cgglobsym(int id);
+int cgloadint(int value, int type);
+int cgloadglob(int id);
+int cgadd(int r1, int r2);
+int cgsub(int r1, int r2);
+int cgmul(int r1, int r2);
+int cgdiv(int r1, int r2);
+void cgprintint(int r);
+int cgstorglob(int r, int id);
+int cgcompare_and_set(AST_node_type ASTop, int r1, int r2);
+void cglabel(int l);
+void cgjump(int l);
+int cgcompare_and_jump(AST_node_type ASTop, int r1, int r2, int label);
+int cgwiden(int r, int oldtype, int newtype);
 
 // stm.c
 ASTnode *function_declaration();
