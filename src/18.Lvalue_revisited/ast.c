ddea2ec3532a7aa849a42e4bcac4f7fc19212b65
chenhang233 <1973433180@qq.com>
2023-10-12 21:45:58 +0800

    feat: 18 ..

--- src/18.Lvalue_revisited/ast.c
+++ src/18.Lvalue_revisited/ast.c
@@ -4,6 +4,8 @@ ASTnode *binexpr(int ptp)
 {
     ASTnode *left, *right;
     ASTnode *l_temp, *r_temp;
+    left = prefix();
+    Token_type type = t_instance.token;
 }
 
 static ASTnode *primary(void)
@@ -38,8 +40,42 @@ static ASTnode *primary(void)
 
 ASTnode *prefix(void)
 {
+    ASTnode *tree;
+    Primitive_type type;
+    switch (t_instance.token)
+    {
+    case T_AMPER:
+        scan(&t_instance);
+        tree = prefix();
+        if (tree->op != A_IDENT && tree->op != A_ADDR)
+            custom_error_int("Get an address that is not an A_IDENT", tree->op);
+        tree->op = A_ADDR;
+        tree->type = pointer_to(tree->type);
+        break;
+    case T_STAR:
+        scan(&t_instance);
+        tree = prefix();
+        if (tree->op != A_IDENT && tree->op != A_DEREF)
+            custom_error_int("Get an address that is not an A_IDENT", tree->op);
+        tree = mkAST_left(A_DEREF, value_at(tree->type), tree, 0);
+        break;
+    default:
+        tree = primary();
+    }
+    return tree;
 }
 
 ASTnode *funccall(void)
 {
+    ASTnode *tree;
+    int id;
+    if (findglob(Text) == -1)
+        custom_error_cahrs("undefined variable", Text);
+
+    match_lparen();
+    tree = binexpr(0);
+    tree = mkAST_left(A_FUNCCALL, Gsym[id].type, tree, id);
+    match_rparen();
+
+    return tree;
 }
