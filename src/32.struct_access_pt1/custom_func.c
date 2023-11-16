a831f3445a441a3f40a192fb15c1ed763b1831e4
chenhang233 <1973433180@qq.com>
2023-11-16 20:58:52 +0800

    feat: 32 init

---
+++ src/32.struct_access_pt1/custom_func.c
@@ -0,0 +1,236 @@
+#include "function.h"
+#include "../cjson/cjson.h"
+
+void custom_error_line()
+{
+    printf("The source code error is on line %d\n", Line);
+    exit(1);
+}
+
+void custom_error_char(char *t, char c)
+{
+    printf("ERROR: %s ,char: %c\n", t, c);
+    custom_error_line();
+}
+
+void custom_error_chars(char *c1, char *c2)
+{
+    printf("ERROR: %s ,info: %s\n", c1, c2);
+    custom_error_line();
+};
+
+void custom_error_int(char *c1, int i)
+{
+    printf("ERROR: %s ,int: %d\n", c1, i);
+    custom_error_line();
+}
+
+void match(Token_type t, char *str)
+{
+    if (t_instance.token == t)
+    {
+        scan(&t_instance);
+    }
+    else
+    {
+        printf("need: %d, your t_instance.token: %d\n", t, t_instance.token);
+        custom_error_chars("connot match", str);
+    }
+}
+
+void match_semi()
+{
+    return match(T_SEMI, ";");
+}
+
+void match_lbrace()
+{
+    match(T_LBRACE, "{");
+}
+
+void match_rbrace()
+{
+    match(T_RBRACE, "}");
+}
+
+void match_lparen()
+{
+    match(T_LPAREN, "(");
+}
+
+void match_rparen()
+{
+    match(T_RPAREN, ")");
+}
+
+void match_ident()
+{
+    match(T_IDENT, "identifier");
+}
+
+void match_int()
+{
+    match(T_INT, "int");
+}
+
+void match_assign()
+{
+    match(T_ASSIGN, "assign");
+}
+
+void match_if()
+{
+    match(T_IF, "if");
+}
+
+void match_while()
+{
+    match(T_WHILE, "while");
+}
+
+void match_for()
+{
+    match(T_FOR, "for");
+}
+
+void match_void()
+{
+    match(T_VOID, "void");
+}
+
+void match_return()
+{
+    match(T_RETURN, "return");
+}
+
+void match_lbracket()
+{
+    match(T_LBRACKET, "[");
+}
+
+void match_rbracket()
+{
+    match(T_RBRACKET, "]");
+}
+
+void match_comma()
+{
+    match(T_COMMA, ",");
+}
+
+static char *AST_node_type_keys[] = {
+    "A_ASSIGN",
+    "A_LOGOR",
+    "A_LOGAND",
+    "A_OR",
+    "A_XOR",
+    "A_AND",
+    "A_EQ",
+    "A_NE",
+    "A_LT",
+    "A_GT",
+    "A_LE",
+    "A_GE",
+    "A_LSHIFT",
+    "A_RSHIFT",
+    "A_ADD",
+    "A_SUBTRACT",
+    "A_MULTIPLY",
+    "A_DIVIDE",
+    "A_INTLIT",
+    "A_STRLIT",
+    "A_IDENT",
+    "A_GLUE",
+    "A_IF",
+    "A_WHILE",
+    "A_FUNCTION",
+    "A_WIDEN",
+    "A_RETURN",
+    "A_FUNCCALL",
+    "A_DEREF",
+    "A_ADDR",
+    "A_SCALE",
+    "A_PREINC",
+    "A_PREDEC",
+    "A_POSTINC",
+    "A_POSTDEC",
+    "A_NEGATE",
+    "A_INVERT",
+    "A_LOGNOT",
+    "A_TOBOOL"};
+static char *Primitive_type_keys[] = {
+    "P_NONE",
+    "P_VOID",
+    "P_CHAR",
+    "P_INT",
+    "P_LONG",
+    "P_VOIDPTR",
+    "P_CHARPTR",
+    "P_INTPTR",
+    "P_LONGPTR"};
+static cJSON *find_next_tree(ASTnode *root, cJSON *json)
+{
+    if (root != NULL)
+    {
+        // cJSON_AddNumberToObject(json, "AST_node_type", root->op);
+        cJSON_AddStringToObject(json, "AST_node_type", AST_node_type_keys[root->op - 1]);
+        cJSON_AddStringToObject(json, "Primitive_type", Primitive_type_keys[root->type]);
+        cJSON_AddNumberToObject(json, "v_id", root->v.id);
+        cJSON_AddNumberToObject(json, "rvalue", root->rvalue);
+
+        cJSON *left = find_next_tree(root->left, cJSON_CreateObject());
+        cJSON *mid = find_next_tree(root->mid, cJSON_CreateObject());
+        cJSON *right = find_next_tree(root->right, cJSON_CreateObject());
+
+        cJSON_AddItemToObject(json, "left", left);
+        cJSON_AddItemToObject(json, "mid", mid);
+        cJSON_AddItemToObject(json, "right", right);
+        return json;
+    }
+    return NULL;
+}
+
+void jsonify_tree(ASTnode *root, const char *filename)
+{
+    FILE *file = fopen(filename, "w");
+    if (file == NULL)
+    {
+        printf("Failed to open file %s\n", filename);
+        return;
+    }
+    cJSON *json = cJSON_CreateObject();
+    if (json == NULL)
+    {
+        printf("Failed to create cJSON object\n");
+        fclose(file);
+        return;
+    }
+    find_next_tree(root, json);
+    char *json_str = cJSON_Print(json);
+    fprintf(file, "%s", json_str);
+    cJSON_Delete(json);
+    free(json_str);
+    fclose(file);
+}
+
+void Gsym_dump(char *name)
+{
+    // if (!(Gsym_dump_file = fopen(name, "w")))
+    //     custom_error_int("open Gsym_dump_file", 0);
+    // for (int i = 0; i < NSYMBOLS; i++)
+    // {
+    //     if (Gsym[i].name == NULL)
+    //     {
+    //         fprintf(Gsym_dump_file, "\tindex=%d\t\tNULL\t\n", i);
+    //         continue;
+    //     }
+    //     fprintf(Gsym_dump_file,
+    //             "\tindex=%d\t\tname=%s\t\
+    //         \tPrimitive_type=%d\t\tStorage_class=%d\t\
+    //         \tendlabel = % d\t\tsize = % d\t\tposn = % d\t\n ",
+    //             i,
+    //             Gsym[i].name, Gsym[i].stype,
+    //             Gsym[i].class, Gsym[i].endlabel,
+    //             Gsym[i].size, Gsym[i].posn);
+    // }
+}
---
+++ src/32.struct_access_pt1/types.c
@@ -0,0 +1,68 @@
+#include "function.h"
+
+int inttype(Primitive_type type)
+{
+    return ((type & 0xf) == 0);
+}
+
+int ptrtype(Primitive_type type)
+{
+    return ((type & 0xf) != 0);
+}
+
+Primitive_type pointer_to(Primitive_type type)
+{ // 0000 0000   位值 64 48 32 16 8 4 2 1
+    if ((type & 0xf) == 0xf)
+        custom_error_int("Unrecognised in pointer_to: type", type);
+    return (type + 1);
+}
+
+Primitive_type value_at(Primitive_type type)
+{
+    if ((type & 0xf) == 0x0)
+        custom_error_int("Unrecognised in value_at: type", type);
+    return (type - 1);
+}
+
+int typesize(Primitive_type type, struct symtable *ctype)
+{
+    if (type == P_STRUCT)
+        return ctype->size;
+    return genprimsize(type);
+}
+
+ASTnode *modify_type(ASTnode *tree, Primitive_type rtype, AST_node_type op)
+{
+    int ltype;
+    int lsize, rsize;
+
+    ltype = tree->type;
+    if (inttype(ltype) && inttype(rtype))
+    {
+        if (ltype == rtype)
+            return tree;
+        lsize = typesize(ltype, NULL);
+        rsize = typesize(rtype, NULL);
+        if (lsize > rsize)
+            return NULL;
+        if (rsize > lsize)
+            return mkAST_left(A_WIDEN, rtype, tree, NULL, 0);
+    }
+    if (ptrtype(ltype))
+    {
+        if (op == 0 && rtype == ltype)
+            return tree;
+    }
+    if (op == A_ADD || op == A_SUBTRACT)
+    {
+        if (inttype(ltype) && ptrtype(rtype))
+        {
+            int rsize = genprimsize(value_at(rtype));
+            if (rsize > 1)
+                return mkAST_left(A_SCALE, rtype, tree, NULL, rsize);
+            else
+                return tree;
+        }
+    }
+    return NULL;
+}
